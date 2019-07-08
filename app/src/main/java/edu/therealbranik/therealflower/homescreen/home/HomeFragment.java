43260a7a448547514c2f7e608bf54868c25844c4
Stefan Jokovic <obcupatio@gmail.com>
2019-07-09 00:29:46 +0200

    HomeFragment - ListView-CardView

--- app/src/main/java/edu/therealbranik/therealflower/homescreen/home/HomeFragment.java
+++ app/src/main/java/edu/therealbranik/therealflower/homescreen/home/HomeFragment.java
@@ -3,11 +3,15 @@ package edu.therealbranik.therealflower.homescreen.home;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import edu.therealbranik.therealflower.R;
 
@@ -16,6 +20,10 @@ import edu.therealbranik.therealflower.R;
  */
 public class HomeFragment extends Fragment {
 
+    ListView listView;
+    CardView cardView;
+    ImageView profile_img,post_img1,post_img2;
+    TextView name,decription,timestamp;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,7 +34,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        listView=view.findViewById(R.id.list_view_home);
+        cardView=view.findViewById(R.id.card_view);
+
+        CardsAdapte
+
+        return view;
     }
 
 }
